c9dd4b3fa6ca522794dfb1b56ffc692f7c8bc4f7
James Deane <jdeane@cloudflare.com>
2022-09-05 17:11:54 +1000

    switch back to object destructuring but with interface

--- src/regex-replace.ts
+++ src/regex-replace.ts
@@ -8,10 +8,16 @@ try {
     `;
   }
   const [src, match, replacement] = Deno.args;
-  console.log(regex_replace(src, match, replacement));
+  console.log(regex_replace({ src, match, replacement }));
 } catch (error) {
   console.error(error);
   Deno.exit(1);
+}
+
+interface RegexReplace {
+  src: string;
+  match: string;
+  replacement: string;
 }
 
 /**
@@ -22,11 +28,7 @@ try {
  * @param match RE2 regex to match on
  * @param replacement string to replace src (can include regex groups)
  */
-function regex_replace(
-  src: string,
-  match: string,
-  replacement: string,
-): string {
+function regex_replace({ src, match, replacement }: RegexReplace): string {
   const regex = new RegExp(match); // convert string to regex
   const replace = replacement.replace(/\{|\}/g, ""); // strip RE2 replacement chars {}
   return src.replace(regex, replace);
