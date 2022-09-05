5d10d53a0167b2ffd84850d70b300f6a17922669
James Deane <jdeane@cloudflare.com>
2022-09-05 15:52:43 +1000

    add regex-replace for testing re2 regex and update README

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # deno
+
 Utility scripts written in Deno 🦕
+
+- [regex-replace.ts](./src/regex-replace.ts)
+  - function to test a RE2-like regex in JavaScript. Supply 3 arguments;
+    `source` string, `match` regex to match with, `replacement`
+  ```
+  e.g. deno run src/regex-replace.ts source match replacement
+  ❯ deno run src/regex-replace.ts '/images/nature/animals/tiger.png' '^/images/[^/]+/[^/]+/(.+)$' '/img/${1}'
+  /img/tiger.png
+  ```
+- [time-requests.ts](./src/time-requests.ts)
+  - fire `x` requests at a URL and log if breaching `y` response time (ms)
+  ```
+  e.g. deno run --allow-net src/time-requests.ts https://example.com y x
+  ❯ deno run --allow-net src/time-requests.ts https://example.com 250 10
+  ```
---
+++ src/regex-replace.ts
@@ -0,0 +1,35 @@
+try {
+  if (!Deno.args.length) {
+    throw `
+    Missing args.
+    
+    Usage:
+    ❯ deno run src/regex-replace.ts '/one/obsolete/two' '^/one/obsolete/(.*)$' '/one/${1}'
+    `;
+  }
+  const [src, match, replacement] = Deno.args;
+  console.log(regex_replace({ src, match, replacement }));
+} catch (error) {
+  console.error(error);
+  Deno.exit(1);
+}
+
+/**
+ * regex_replace: function to test a RE2 regex in JavaScript
+ *
+ * ! Extremely simplistic implementation, use for quick sanity checks
+ * @param src initial string to operate on
+ * @param match RE2 regex to match on
+ * @param replacement string to replace src (can include regex groups)
+ */
+function regex_replace(
+  { src, match, replacement }: {
+    src: string;
+    match: string;
+    replacement: string;
+  },
+): string {
+  const regex = new RegExp(match); // convert string to regex
+  const replace = replacement.replace(/\{|\}/g, ""); // strip RE2 replacement chars {}
+  return src.replace(regex, replace);
+}
